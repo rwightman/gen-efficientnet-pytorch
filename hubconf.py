dc54f9a6801451604f5256a471f7ef3d19b40f63
Ross Wightman <rwightman@gmail.com>
2019-06-13 09:20:31 -0700

    Update hubconf

--- hubconf.py
+++ hubconf.py
@@ -1,6 +1,7 @@
-dependencies = ['torch']
+dependencies = ['torch', 'math']
 
 from gen_efficientnet import efficientnet_b0
 from gen_efficientnet import mobilenetv3_100
-from gen_efficientnet import semnasnet_100
+from gen_efficientnet import mnasnet_a1
+from gen_efficientnet import mnasnet_b1
 from gen_efficientnet import fbnetc_100
