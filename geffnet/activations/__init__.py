a4ac4dd7d72069a2aa4564df53ff4eb9b9f64400
Ross Wightman <rwightman@gmail.com>
2020-11-29 21:49:27 -0800

    Workaround for native SiLU (Swish) not supporting ONNX export. Fix #55

--- geffnet/activations/__init__.py
+++ geffnet/activations/__init__.py
@@ -100,14 +100,17 @@ def get_act_fn(name='relu'):
     """
     if name in _OVERRIDE_FN:
         return _OVERRIDE_FN[name]
-    no_me = config.is_exportable() or config.is_scriptable() or config.is_no_jit()
-    if not no_me and name in _ACT_FN_ME:
+    use_me = not (config.is_exportable() or config.is_scriptable() or config.is_no_jit())
+    if use_me and name in _ACT_FN_ME:
         # If not exporting or scripting the model, first look for a memory optimized version
         # activation with custom autograd, then fallback to jit scripted, then a Python or Torch builtin
         return _ACT_FN_ME[name]
-    no_jit = config.is_exportable() or config.is_no_jit()
+    if config.is_exportable() and name in ('silu', 'swish'):
+        # FIXME PyTorch SiLU doesn't ONNX export, this is a temp hack
+        return swish
+    use_jit = not (config.is_exportable() or config.is_no_jit())
     # NOTE: export tracing should work with jit scripted components, but I keep running into issues
-    if no_jit and name in _ACT_FN_JIT:  # jit scripted models should be okay for export/scripting
+    if use_jit and name in _ACT_FN_JIT:  # jit scripted models should be okay for export/scripting
         return _ACT_FN_JIT[name]
     return _ACT_FN_DEFAULT[name]
 
@@ -119,11 +122,15 @@ def get_act_layer(name='relu'):
     """
     if name in _OVERRIDE_LAYER:
         return _OVERRIDE_LAYER[name]
-    no_me = config.is_exportable() or config.is_scriptable() or config.is_no_jit()
-    if not no_me and name in _ACT_LAYER_ME:
+    use_me = not (config.is_exportable() or config.is_scriptable() or config.is_no_jit())
+    if use_me and name in _ACT_LAYER_ME:
         return _ACT_LAYER_ME[name]
-    no_jit = config.is_exportable() or config.is_no_jit()
-    if not no_jit and name in _ACT_LAYER_JIT:  # jit scripted models should be okay for export/scripting
+    if config.is_exportable() and name in ('silu', 'swish'):
+        # FIXME PyTorch SiLU doesn't ONNX export, this is a temp hack
+        return Swish
+    use_jit = not (config.is_exportable() or config.is_no_jit())
+    # NOTE: export tracing should work with jit scripted components, but I keep running into issues
+    if use_jit and name in _ACT_FN_JIT:  # jit scripted models should be okay for export/scripting
         return _ACT_LAYER_JIT[name]
     return _ACT_LAYER_DEFAULT[name]
 
