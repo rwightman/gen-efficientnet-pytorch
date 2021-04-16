eb874f06e177890d7f78c28cea1a673dcd245c60
instinct79 <43484291+instinct79@users.noreply.github.com>
2021-04-16 11:39:01 -0700

    Update conv2d_layers.py
    Resolve container_abcs not found in torch._six, for pyTorch 1.8

--- geffnet/conv2d_layers.py
+++ geffnet/conv2d_layers.py
@@ -8,7 +8,14 @@ Copyright 2020 Ross Wightman
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch._six import container_abcs
+
+TORCH_MAJOR = int(torch.__version__.split('.')[0])
+TORCH_MINOR = int(torch.__version__.split('.')[1])
+
+if TORCH_MAJOR == 1 and TORCH_MINOR < 8:
+    from torch._six import container_abcs
+else:
+    import collections.abc as container_abcs
 
 from itertools import repeat
 from functools import partial
